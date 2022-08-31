1f8cffaf1392fac0bbba0ea6d24989e8dc72a6c8
johnpascual <ashleypascual@hotmail.es>
2022-08-31 09:53:27 +0100

    hello world

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
+
+const App = () => {
+
+    return <h1>Hello, World!</h1>
+}
+
+export default App;
