bf129de90fe52d30a4003593b3ae3b732ed9c8af
johnpascual <ashleypascual@hotmail.es>
2022-08-31 15:20:40 +0100

    card styles

--- src/components/RepoCard/index.jsx
+++ src/components/RepoCard/index.jsx
@@ -6,8 +6,8 @@ const RepoCard = ({data}) => {
 
     return <div>
             <h2>This user has {data.length} repositories</h2>
-            <div>
-               {data.map(i => <div>{i.name}</div>)}
+            <div className="container">
+               {data.map(i => <div className="repo-card"><a href={i.url}><h3>{i.name}</h3></a><p>Stargazers: {i.stargazers_count}</p><p>Watchers: {i.watchers_count}</p><p>Forks: {i.forks_count}</p><p>Issues: {i.open_issues_count}</p></div>)}
             </div>
             </div>
 }
