412b82fa2521841ee1bf2fdbf0f448538520476e
“yourGithubUsername” <mohammedSIGoni@gmail.com>
2022-08-31 14:42:12 +0100

    fixed repocard

--- src/components/RepoCard/index.jsx
+++ src/components/RepoCard/index.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const RepoCard = ({data}) => {
 
-    console.log(data[0])
+    console.log(data)
 
     return <div>
             <h2>This user has {data.length} repositories</h2>
+            <div>
+               {data.map(i => <div>{i.name}</div>)}
+            </div>
             </div>
 }
 
